b6f9c9728bd6e0bf3bc6f86c8bd3592efe82cb0b
dist <120486581+dimitarpst@users.noreply.github.com>
2024-07-20 20:08:08 +0300

    -added cv view

--- cv.php
+++ cv.php
@@ -1,120 +1,243 @@
-﻿<!doctype html>
-<html lang="en">
-
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title> Free Dental Medical Hospital Website Template | Smarteyeapps.com</title>
-
-    <link rel="shortcut icon" href="img/fav.jpg">
-    <link rel="stylesheet" type="text/css" href="css/cv/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="css/cv/fontawsom-all.min.css">
-    <link rel="stylesheet" type="text/css" href="css/cv/styleCV.css" />
+  <link href="https://unpkg.com/boxicons@latest/css/boxicons.min.css" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Archivo+Narrow&family=Julius+Sans+One&family=Open+Sans&family=Source+Sans+Pro&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="css/cv.css">
 </head>
-
 <body>
-    <div class="container-fluid overcover">
-        <div class="container profile-box">
-            <div class="row">
-                <div class="col-md-4 left-co">
-                    <div class="left-side">
-                        <div class="profile-info">                            
-                            <h2 id="fullname">Fullname</h2>
-                            <h5>Gender</h5>
-                            <h6>Age</h6>
-                        </div>
-                        <h4 class="ltitle">Contact</h4>
-                        <div class="contact-box pb0">
-                            <div class="icon">
-                                <i class="fas fa-phone"></i>
-                            </div>
-                            <div class="detail">
-                                phone
-                            </div>
-                        </div>
-                        <div class="contact-box pb0">
-                            <div class="icon">
-                                <i class="fas fa-globe-americas"></i>
-                            </div>
-                            <div class="detail">
-                                email <br>
-                            </div>
-                        </div>
-
-                        
-                    </div>
-                </div>
-                <div class="col-md-8 rt-div">
-
-                        <h2 class="rit-titl"><i class="fas fa-briefcase"></i> Work Experiance</h2>
-                        <div class="work-exp">
-                            <ul>
-                                <li><i class="far fa-hand-point-right"></i> Job Title </li>
-                                <li><i class="far fa-hand-point-right"></i> Company </li>
-                                <li><i class="far fa-hand-point-right"></i> Start Date </li>
-                                <li><i class="far fa-hand-point-right"></i> End Date </li>
-                            </ul>
-                        </div>
-
-                        <h2 class="rit-titl"><i class="fas fa-graduation-cap"></i> Education</h2>
-                        <div class="education">
-                            <ul class="row no-margin">
-                                <li class="col-md-6">
-                                    Degree</li>
-                                <li class="col-md-6">
-                                    University</li>
-                                <li class="col-md-6">
-                                    Graduation Year</li>
-                                
-                            </ul>
-                        </div>
-
-
-                        </div>
-                    </div>
-                </div>
+  <page size="A4">
+    <div class="container">
+      <div class="leftPanel">
+        <img src="img/img.png"/>
+        <div class="details">
+          <div class="item bottomLineSeparator">
+            <h2>
+              CONTACT
+            </h2>
+            <div class="smallText">
+              <p><i class='bx bxs-phone'></i> Phone</p>
+              <p><i class='bx bxs-envelope' ></i><a href="email"> Email</a></p>  
             </div>
+          </div>
+          <div class="item bottomLineSeparator">
+            <h2>
+              Details
+            </h2>
+            <div class="smallText">
+            
+              <p><i class='bx bx-male-female'></i> Gender</p>
+              <p><i class='bx bx-time-five'></i> Age</p>
+            </div>
+          </div>
+          <div class="item bottomLineSeparator">
+            <h2>
+              SKILLS
+            </h2>
+            <div class="smallText">
+              <div class="skill">
+                <div>
+                  <span>Accounting</span>
+                </div>
+                <div class="yearsOfExperience">
+                  <span class="alignright">14</span>
+                  <span class="alignleft">years</span>
+                </div>
+              </div>
+
+              <div class="skill">
+                <div>
+                  <span>Word</span>
+                </div>
+                <div class="yearsOfExperience">
+                  <span class="alignright">3</span>
+                  <span class="alignleft">years</span>
+                </div>
+              </div>
+
+              <div class="skill">
+                <div>
+                  <span>Powerpoint</span>
+                </div>
+                <div class="yearsOfExperience">
+                  <span class="alignright">3</span>
+                  <span class="alignleft">years</span>
+                </div>
+              </div>
+
+              <div class="skill">
+                <div>
+                  <span>Accounting</span>
+                </div>
+                <div class="yearsOfExperience">
+                  <span class="alignright">2</span>
+                  <span class="alignleft">years</span>
+                </div>
+              </div>
+
+              <div class="skill">
+                <div>
+                  <span>Marketing</span>
+                </div>
+                <div class="yearsOfExperience">
+                  <span class="alignright">2</span>
+                  <span class="alignleft">years</span>
+                </div>
+              </div>
+
+              <div class="skill">
+                <div>
+                  <span>Photoshop</span>
+                </div>
+                <div class="yearsOfExperience">
+                  <span class="alignright">2</span>
+                  <span class="alignleft">years</span>
+                </div>
+              </div>
+
+              <div class="skill">
+                <div>
+                  <span>French</span>
+                </div>
+                <div class="yearsOfExperience">
+                  <span class="alignright">2</span>
+                  <span class="alignleft">years</span>
+                </div>
+              </div>
+
+              <div class="skill">
+                <div>
+                  <span>English</span>
+                </div>
+                <div class="yearsOfExperience">
+                    <span class="alignright">1</span>
+                    <span class="alignleft">year</span>
+                </div>
+              </div>
+
+              <div class="skill">
+                <div>
+                  <span>Management</span>
+                </div>
+                <div class="yearsOfExperience">
+                  <span>1 year</span>
+                </div>
+              </div>
+
+            </div>
+          </div>
+          <div class="item">
+            <h2>
+              EDUCATION
+            </h2>
+            <div class="smallText">
+              <p><i class='bx bxs-graduation'></i> Degree</p>
+              <p><i class='bx bx-building-house'></i> University</p>
+              <p><i class='bx bx-calendar'></i> Graduation year</p>
+            </div>
+          </div>
         </div>
+        
+      </div>
+      <div class="rightPanel">
+        <div>
+          <h1>
+            Full Name
+          </h1>
+          <div class="smallText">
+            <h3>
+              Job Title
+            </h3>
+          </div>
+        </div>
+        <div>
+          <h2>
+            About me
+          </h2>
+          <div class="smallText">
+            <p>
+               About me text
+            </p>
+          </div>
+        </div>
+        <div class="workExperience">
+          <h2>
+            Work experience
+          </h2>
+          <ul>
+
+            <li>
+              <div class="jobPosition">
+                <span class="bolded">
+                  Job Title
+                </span>
+                <span>
+                  Start Date - End Date
+                </span>
+              </div>
+              <div class="projectName bolded">
+                <span>
+                  Company
+                </span>
+              </div>
+              <div class="smallText">
+                <p>
+                  Maecenas eget semper sapien. Sed convallis nunc egestas dui convallis dictum id nec metus. Donec vestibulum justo mauris, ac congue lacus tincidunt id. Vivamus rhoncus risus ac ex varius gravida. Donec eget ullamcorper ipsum.
+                </p>
+  
+                <ul>
+                  <li>
+                    <p>
+                      Maecenas auctor euismod felis vel semper. Nulla facilisi. Quisque quis odio dui. Morbi venenatis magna quis libero mollis facilisis. Ut semper, eros eu dictum efficitur, ligula felis aliquet ante, sed commodo odio nisi a augue. 
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      Curabitur at interdum nunc, nec sodales nulla. Nulla facilisi. Nam egestas risus sed maximus feugiat. Sed semper arcu ac dui consectetur consectetur. Nulla dignissim nec turpis id rhoncus. In hac habitasse platea dictumst. 
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      Nunc iaculis mauris nec viverra placerat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam erat volutpat. Vivamus sed ex et magna volutpat sodales et sed odio. 
+                    </p>
+                  </li>
+                </ul>
+
+              </div>
+            </li>
+
+            <li>
+              <div class="jobPosition">
+                <span class="bolded">
+                  Job Title
+                </span>
+                <span>
+                  Start Date - End Date
+                </span>
+              </div>
+              <div class="projectName bolded">
+                <span>
+                  Company
+                </span>
+              </div>
+              <div class="smallText">
+                <p>
+                  Quisque rutrum mollis ornare. In pharetra diam libero, non interdum dui imperdiet quis. Quisque aliquam sapien in libero finibus sodales. Mauris id commodo metus. In in laoreet dolor.
+                </p>
+                <ul>
+                  <li>
+                    Integer commodo ullamcorper mauris, id condimentum lorem elementum sed. Etiam rutrum eu elit ut hendrerit. Vestibulum congue sem ac auctor semper. Aenean quis imperdiet magna. Sed eget ullamcorper enim. Vestibulum consequat turpis eu neque efficitur blandit sed sit amet neque. Curabitur congue semper erat nec blandit.
+                  </li>
+                </ul>
+
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
+  </page>
 </body>
-
-<script src="/js/cvstuff/jquery-3.2.1.min.js"></script>
-<script src="/js/cvstuff/popper.min.js"></script>
-<script src="/js/cvstuff/bootstrap.min.js"></script>
-<script src="/js/cvstuff/script.js"></script>
-<script>// Update the template with actual CV information
-window.addEventListener('DOMContentLoaded', async () => {
-    try {
-        const response = await fetch('/api/cv', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + sessionStorage.getItem('token')
-            },
-        });
-
-        if (response.ok) {
-            const cvData = await response.json();
-
-            // Populate the template with CV information
-            document.getElementById('fullname').textContent = cvData.FullName;
-            document.getElementById('gender').textContent = cvData.Gender;
-            document.getElementById('age').textContent = cvData.Age;
-            document.getElementById('phone').textContent = cvData.Phone;
-            document.getElementById('email').textContent = cvData.Email;
-            document.getElementById('job-title').textContent = cvData.Jobtitle;
-            document.getElementById('company').textContent = cvData.Company;
-            document.getElementById('start-date').textContent = cvData.StartDate;
-            document.getElementById('end-date').textContent = cvData.EndDate;
-            document.getElementById('degree').textContent = cvData.Degree;
-            document.getElementById('university').textContent = cvData.University;
-            document.getElementById('graduation-year').textContent = cvData.GraduationYear;
-        } else {
-            console.error('Failed to fetch CV data:', response.status);
-        }
-    } catch (error) {
-        console.error('Error fetching CV data:', error);
-    }
-});</script>
-
 </html>
